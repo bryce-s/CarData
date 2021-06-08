c63598a4e3e54666be3c528e93be74dd65cb3024
bryce-s <bryces@umich.edu>
2021-06-07 20:53:07 -0400

    generates all manufacturer json files

--- carBrandsPerState.py
+++ carBrandsPerState.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 import csv
-from os import stat
+import os
 import regionToState
+from stateToId import getValFromState
 import json
+
 
 with open('./vehicles.csv', 'r') as read_obj:
     csv_reader = csv.reader(read_obj)
@@ -25,35 +27,28 @@ manufacturers = set([brand for state in stateToBrandToCount.keys() for brand in 
 stateToTotalVehicles = dict({state: sum(stateToBrandToCount[state].values()) for state in stateToBrandToCount.keys()})
 
 stateBrandToPercent = defaultdict(lambda: defaultdict(lambda: 0))
-for state in stateToBrandToCount.keys():
+for state in filter(lambda x: x != "washington, DC", stateToBrandToCount.keys()):
     for brand in stateToBrandToCount[state].keys():
         count: int = stateToBrandToCount[state][brand]
         stateBrandToPercent[state][brand] = count / stateToTotalVehicles[state] 
-    
 
 def BrandPercentages(stateBrandToPercent: dict, brand: str):
     stateId = 1
     results = sorted([
-       {
-        "id": state,
-        "percentage": str(stateBrandToPercent[state][brand]),
-        "val": str(idx+1) if idx+1 > 10 else f'0{idx+1}' 
-       }  
-        for idx, state in enumerate(sorted(stateBrandToPercent.keys())) 
-      ], key=lambda x: x["percentage"])
-    resJsonStr = json.dumps(results, indent=2)
-    print(resJsonStr)
-BrandPercentages(stateBrandToPercent, 'ford')
-BrandPercentages(stateBrandToPercent, 'toyota')
-# print("MI")
-# print(stateBrandToPercent["MI"])
-# print("NY")
-# print(stateBrandToPercent["NY"])
-# print("TX")
-# print(stateBrandToPercent["TX"])
+        {
+            "id": state,
+            "percentage": str(stateBrandToPercent[state][brand]),
+            "numericId": str(idx+1) if idx+1 > 10 else f'0{idx+1}',
+            "val": getValFromState(state)
+        }
+        for idx, state in enumerate(sorted(stateBrandToPercent.keys()))
+    ], key=lambda x: x["percentage"])
+    jsonRes: str = json.dumps(results, indent=2)
+    filename = f'./{brand}.json'
+    if os.path.exists(filename):
+        os.remove(filename)
+    with open(filename, "w") as outputFile:
+        outputFile.write(jsonRes)
 
-print('manufacturers')
-print(manufacturers)
-
-
-
+any( BrandPercentages(stateBrandToPercent, brand) for brand in manufacturers )
+    
